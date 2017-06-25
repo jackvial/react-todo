a6e82aede763c96a10df4adfd27f9cc63a0d026e
Jack Vial <vialjack@gmail.com>
2017-06-25 15:40:02 -0400

    moved state to list component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import List from './ListComponent'
+import List from './ListComponent';
 
-let todos = [
-  {
-    title: 'One',
-    content: 'bananas'
-  },
-  {
-    title: 'Two',
-    content: 'cheese burger'
-  },
-  {
-    title: 'Three',
-    content: 'apples'
-  }
-];
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <List todos={todos}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <List />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/ListComponent.js
+++ src/ListComponent.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import ListItem from './ListItemComponent'
+import ListItem from './ListItemComponent';
+
+let todos = [
+    {
+        title: 'One',
+        content: 'bananas'
+    },
+    {
+        title: 'Two',
+        content: 'cheese burger'
+    },
+    {
+        title: 'Three',
+        content: 'apples'
+    }
+];
 
 class List extends Component {
-  render() {
-    console.log(this.props);
-    let todos = this.props.todos;
-    let items = todos.map((item, index) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: todos
+        };
+    }
+    addTodo(e) {
+        e.preventDefault();
+        console.log(e);
+    }
+    render() {
+        let items = this.state.todos.map((item, index) => {
+            return <ListItem key={index} title={item.title} content={item.content} />;
+        });
         return (
-            <ListItem key={index} title={item.title} content={item.content}/>
+            <div>
+                <input type="text" value={this.state.newTodo} />
+                <button type="button" onClick={this.addTodo}>Add</button>
+                <div className="list">
+                    {items}
+                </div>
+            </div>
         );
-    });
-    return (
-      <div className="list">
-        {items}
-      </div>
-    );
-  }
+    }
 }
 
 export default List;
